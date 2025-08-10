95c761f7bb62778763af06bd4c32ca8ee52660ae
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-08-10 14:54:36 +0300

    style: impact by the number component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ImpactStats from './components/ImpactStats'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+    <ImpactStats />
   )
 }
 
