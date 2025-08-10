884e5d461ca61dcdcfe8851d95088fe2c7ec87d5
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-08-10 22:59:55 +0300

    style: meet our experts component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import AboutSection from './components/DesignThinkingHub'
 import ImpactStats from './components/ImpactStats'
 import NewsletterSignup from './components/NewsletterSignup'
 import ProgramTracks from './components/ProgramTracks'
+import MeetOurExpertsSection from './components/MeetExpertsSection'
+import MeetOurExpertsPage from './pages/MeetOurExpertsPage'
 
 const App = () => {
   return (
     <>
       <AboutSection />
+      <MeetOurExpertsSection />
+      <MeetOurExpertsPage/>
       <ProgramTracks />
       <ImpactStats />
       <NewsletterSignup />
